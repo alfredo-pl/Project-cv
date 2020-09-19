855c046f5d1cd54938d8e46fc6c31fa575127f05
alfredo <aalfredoplz@gmail.com>
2020-09-19 12:10:33 -0300

    First commit proyect cv

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(function () {
+  //skills
+  $('.circlechart').circlechart();
+
+  //carousel poppover
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
+
+});
